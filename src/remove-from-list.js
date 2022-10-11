a4f6750e39b2b2ed73e11cf38fff3846cf904456
Milsevka <lusindami99@gmail.com>
2022-10-11 23:16:07 +0300

    feat: add list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (!l) {
+    return null;
+  }
+  if (l.value === k) {
+    return removeKFromList(l.next, k);
+  } else {
+    let list = new ListNode(l.value);
+    list.next = removeKFromList(l.next, k)
+    return list
+  }
 }
 
 module.exports = {
